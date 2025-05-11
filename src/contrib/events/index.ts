0416ad35ce6d541521c54a0190f9bce880618b9d
Mikhail Remmele <mikhail.remmele@gmail.com>
2025-05-11 16:35:59 +0300

    fix world event types

--- src/contrib/events/index.ts
+++ src/contrib/events/index.ts
@@ -1,7 +1,7 @@
 import type { Actor } from '../../engine/actor';
 import type { Vector2 } from '../../engine/math-lib';
 import type { CustomMouseEvent, CustomKeyboardEvent } from '../types/input-events';
-import type { ActorEvent, SceneEvent } from '../../types/events';
+import type { ActorEvent, SceneEvent, WorldEvent } from '../../types/events';
 
 export const GameStatsUpdate = 'GameStatsUpdate';
 export const Collision = 'Collision';
@@ -18,25 +18,19 @@ export const PlayAudio = 'PlayAudio';
 export const StopAudio = 'StopAudio';
 export const SetAudioVolume = 'SetAudioVolume';
 
-export type MouseInputEvent = SceneEvent<CustomMouseEvent>;
+export type MouseInputEvent = WorldEvent<CustomMouseEvent>;
 
-export type KeyboardInputEvent = SceneEvent<CustomKeyboardEvent>;
+export type KeyboardInputEvent = WorldEvent<CustomKeyboardEvent>;
 
-export type GameStatsUpdateEvent = SceneEvent<{
-  fps: number
-  actorsCount: number
-}>;
+export type GameStatsUpdateEvent = WorldEvent<{ fps: number, actorsCount: number }>;
+
+export type SetAudioGroupVolumeEvent = WorldEvent<{ group: string, value: number }>;
 
 export type CollisionEvent = SceneEvent<{
   actor1: Actor
   actor2: Actor
   mtv1: Vector2
   mtv2: Vector2
-}>;
-
-export type SetAudioGroupVolumeEvent = SceneEvent<{
-  group: string
-  value: number
 }>;
 
 export type MouseControlEvent<T = Record<string, never>>
