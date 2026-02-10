43d0437560f7c3e9c45bcfe1beeed80507154267
Mikhail Remmele <mikhail.remmele@gmail.com>
2026-02-11 00:28:28 +0300

    fix texture size uniform pass and add own copy of mesh geometry for each mesh

--- src/contrib/systems/renderer/builders/mesh-builder/consts.ts
+++ src/contrib/systems/renderer/builders/mesh-builder/consts.ts
@@ -1,7 +1,2 @@
-import { MeshGeometry } from 'pixi.js';
-
-export const UNIT_QUAD_GEOMETRY = new MeshGeometry({
-  positions: new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]),
-  uvs: new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]),
-  indices: new Uint32Array([0, 1, 2, 0, 2, 3]),
-});
+export const GEOMETRY_POSITIONS = new Float32Array([0, 0, 1, 0, 1, 1, 0, 1]);
+export const GEOMETRY_INDICES = new Uint32Array([0, 1, 2, 0, 2, 3]);
